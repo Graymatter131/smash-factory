45a76ac8cc23b780217b5b0eea8205a40534a022
Graymatter131 <154765532+Graymatter131@users.noreply.github.com>
2026-05-07 22:14:41 -0600

    adding shit

---
+++ dan.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+
+const SPEED = 30000.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	
+
+
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_vector("left", "right", "up", "down")
+	
+	if direction:
+		velocity = direction * SPEED * delta
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
+		velocity.y = move_toward(velocity.y, 0, SPEED * delta)
+	move_and_slide()
