2ce283377ee969736a3a78808965c2fee1647695
Graymatter131 <154765532+Graymatter131@users.noreply.github.com>
2026-05-10 22:38:44 -0600

    add

--- area_test.gd
+++ area_test.gd
@@ -1,5 +1,5 @@
 extends Area2D
-
+signal run
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_mouse_entered() -> void:
+	pass # Replace with function body.
